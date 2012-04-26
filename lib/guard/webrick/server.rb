3a160d761642c59b8768d480dd53c4b975b7325d
Derek Prior <derekprior@gmail.com>
2012-04-26 17:00:43 -0300

    use non-caching server

--- lib/guard/webrick/server.rb
+++ lib/guard/webrick/server.rb
@@ -13,6 +13,19 @@ module Guard
           :DocumentRoot => File.expand_path(options[:docroot])
         )
       end
+      
+      def prevent_caching(res)
+        res['ETag']          = nil
+        res['Last-Modified'] = Time.now + 100**4
+        res['Cache-Control'] = 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0'
+        res['Pragma']        = 'no-cache'
+        res['Expires']       = Time.now - 100**4
+      end
+
+      def do_GET(req, res)
+        super
+        prevent_caching(res)
+      end
 
       def start
         %w{TERM HUP}.each { |signal| trap(signal){ server.shutdown } }
